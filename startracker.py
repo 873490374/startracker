6dd6c10a0404e28fd985e9d092ff9573387d2ded
Your Name <szyy89@gmail.com>
2015-04-26 21:19:30 +0200

    Added thresholding

--- startracker.py
+++ startracker.py
@@ -57,13 +57,14 @@ of attitude
 - Image centroiding (possible techniques:
     - Weighted Sum,
     - Maximum Likelihood Estimator)             <---
-2. Celestial to xy
+2. Celestial to xy                              <---
 3. Pattern Matching
     - Angle Matching,
     - Spherical Triangle Pattern Matching,
     - Planar Triangle Pattern Matching,         <---
     - Rate Matching
-4. Attitude calculation - quaternion
+4. k-vector technique                           <---
+5. Attitude calculation - quaternion
     - The Predictive Attitude Determination,
     - QUEST,                                    <---
     - TRIAD,
@@ -71,3 +72,29 @@ of attitude
     - The Fast Optimal
     - Attitude Matrix
 """
+
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('1.jpg', 0)
+
+ret, thresh1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+ret, thresh2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
+ret, thresh3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC)
+ret, thresh4 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
+ret, thresh5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
+
+thresh = ['img', 'thresh1', 'thresh2', 'thresh3', 'thresh4', 'thresh5']
+
+for i in range(6):
+    plt.subplot(2, 3, i+1), plt.imshow(eval(thresh[i]), 'gray')
+    plt.title(thresh[i])
+
+img2 = plt.show()
+from matplotlib.backends.backend_pdf import PdfPages
+pp = PdfPages('multipage.pdf')
+plt.savefig(pp, format='pdf')
+
+cv2.imwrite('result.jpeg', thresh1)
