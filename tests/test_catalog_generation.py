fd01d2dd2ab571ebf66394d1492a1492a3daf08b
Szymon <szymon.michalski89@gmail.com>
2018-10-01 16:02:41 +0200

    Corrected catalog generation test for parallel catalog generation

--- tests/test_catalog_generation.py
+++ tests/test_catalog_generation.py
@@ -1,47 +1,31 @@
-import csv
+import numpy as np
 import os
 
-import pytest
 
-from program.catalog.catalog_generator import CatalogGenerator
-from program.const import MAIN_PATH, SENSOR_VARIANCE, CAMERA_FOV
-from program.planar_triangle import ImagePlanarTriangle
-from program.star import StarUV
+from program.parallel.triangle_catalog_generator_parallel import (
+    TriangleCatalogGeneratorParallel,
+)
+from program.const import MAIN_PATH
 
 
 class TestCatalogGeneration:
     def test_catalog_generation(self):
-        generator = CatalogGenerator(
-            max_magnitude=2, sensor_variance=SENSOR_VARIANCE,
-            camera_fov=CAMERA_FOV)
-        catalog = generator.generate_triangles(
-            os.path.join(MAIN_PATH, 'program/validation/data/hip_main.dat'))
-        cat = []
-        with open(os.path.join(
-                MAIN_PATH, 'tests/catalog/catalog_generation_test_data.csv'),
-                'r', newline='') as csvfile:
-            csvreader = csv.DictReader(csvfile)
-            for row in csvreader:
-                cat.append(ImagePlanarTriangle(
-                    StarUV(int(row['star1_id']), None, None),
-                    StarUV(int(row['star2_id']), None, None),
-                    StarUV(int(row['star3_id']), None, None),
-                    float(row['area']), float(row['moment']),
-                    None, None))
+        star_catalog_fp = os.path.join(
+            MAIN_PATH, 'program/validation/data/hip_main.dat')
+        expected_data_fp = os.path.join(
+            MAIN_PATH, 'tests/catalog/catalog_generation_test_data.csv')
+        generator = TriangleCatalogGeneratorParallel(max_magnitude=2)
+        catalog = generator.generate_triangles(star_catalog_fp)
+        with open(expected_data_fp, 'rb') as f:
+            cat = np.genfromtxt(f, dtype=np.float64, delimiter=',')
         assert len(catalog) == 5
         assert len(catalog) == len(cat)
-        i = 0
         for i in range(len(catalog)):
             t1 = catalog[i]
             t2 = cat[i]
-            assert t1.s1.id == t2.s1.id
-            assert t1.s2.id == t2.s2.id
-            assert t1.s3.id == t2.s3.id
-            assert t1.area == t2.area
-            assert t1.moment == t2.moment
-        # generator.save_to_file(catalog, os.path.join(
-        #     MAIN_PATH, 'tests/catalog/catalog_generation_test_data.csv'))
-
-    @pytest.mark.skip('Not ready')
-    def test_save_catalog(self):
-        pass
+            assert t1[0] == t2[0]
+            assert t1[1] == t2[1]
+            assert t1[2] == t2[2]
+            assert t1[3] == t2[3]
+            assert t1[4] == t2[4]
+        # np.savetxt(expected_data_fp, catalog, delimiter=',')
