9f1cf31bb00342bf945c3790f1a6ebfed4c0268f
Szymon Michalski <szymon.michalski89@gmail.com>
2018-12-05 23:22:54 +0100

    WIP tracking mode

---
+++ program/tracker/tracking_mode.py
@@ -0,0 +1,18 @@
+# TODO create tracking mode
+
+"""
+Input:
+previous_found_stars
+new_image_stars
+
+alg:
+make list of all possible triangles for prev and new
+compare triangles
+if any triangle matched:
+    try to match all other stars (look for only stars with angle in FOV)
+
+"""
+
+"""
+make tests for both positive and negative following
+"""
