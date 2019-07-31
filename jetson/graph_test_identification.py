a4130193ca1baab37733744feab3bffae657d886
Szymon Michalski <szymon.michalski89@gmail.com>
2019-07-31 16:17:14 +0200

    Clean graph code, add test image

--- jetson/graph_test_identification.py
+++ jetson/graph_test_identification.py
@@ -15,50 +15,52 @@ avg_time = []
 # exact_good_verify.append(982 / 10)
 # percent_identified_stars.append(99.69)
 # avg_time.append(0.009)
+#
+# test_names.append('mag5_uv_cat_mag5')
+# in_scene_good_verify.append(1000 / 10)
+# in_triangle_good_verify.append(993 / 10)
+# exact_good_verify.append(993 / 10)
+# percent_identified_stars.append(99.17)
+# avg_time.append(0.0229)
 
-test_names.append('mag5_uv_cat_mag5')
-in_scene_good_verify.append(1000 / 10)
-in_triangle_good_verify.append(993 / 10)
-exact_good_verify.append(993 / 10)
-percent_identified_stars.append(99.17)
-avg_time.append(0.0229)
+test_names.append('mag4 cat5')
+in_scene_good_verify.append(972 / 10)
+in_triangle_good_verify.append(929 / 10)
+exact_good_verify.append(929 / 10)
+percent_identified_stars.append(95.03)
+avg_time.append(0.0079)
 
-test_names.append('mag556_uv_cat_mag5')
-in_scene_good_verify.append(922 / 10)
-in_triangle_good_verify.append(904 / 10)
-exact_good_verify.append(904 / 10)
-percent_identified_stars.append(32.2)
-avg_time.append(0.0401)
-
-test_names.append('mag556_uv_cat_mag6')
-in_scene_good_verify.append(984 / 10)
-in_triangle_good_verify.append(979 / 10)
-exact_good_verify.append(979 / 10)
-percent_identified_stars.append(95.47)
-avg_time.append(2.193535611206003)
-
-# test_names.append('mag4_xy_cat5')
-# in_scene_good_verify.append(972 / 10)
-# in_triangle_good_verify.append(929 / 10)
-# exact_good_verify.append(929 / 10)
-# percent_identified_stars.append(95.03)
-# avg_time.append(0.0079)
-
-test_names.append('mag5_xy_cat5')
+test_names.append('mag5 cat5')
 in_scene_good_verify.append(996 / 10)
 in_triangle_good_verify.append(961 / 10)
 exact_good_verify.append(960 / 10)
 percent_identified_stars.append(95.89)
 avg_time.append(0.0226)
 
-test_names.append('esa_xy_cat_mag5')
+# test_names.append('mag556_uv_cat_mag5')
+test_names.append('mag5.56 cat5')
+in_scene_good_verify.append(922 / 10)
+in_triangle_good_verify.append(904 / 10)
+exact_good_verify.append(904 / 10)
+percent_identified_stars.append(32.2)
+avg_time.append(0.0401)
+
+# test_names.append('mag556_uv_cat_mag6')
+test_names.append('mag5.56 cat6')
+in_scene_good_verify.append(984 / 10)
+in_triangle_good_verify.append(979 / 10)
+exact_good_verify.append(979 / 10)
+percent_identified_stars.append(95.47)
+avg_time.append(2.193535611206003)
+
+test_names.append('esa cat5')
 in_scene_good_verify.append(90)
 in_triangle_good_verify.append(90)
 exact_good_verify.append(90)
 percent_identified_stars.append(44.76)
 avg_time.append(0.0871)
 
-test_names.append('esa_xy_cat_mag6')
+test_names.append('esa cat6')
 in_scene_good_verify.append(66)
 in_triangle_good_verify.append(66)
 exact_good_verify.append(66)
@@ -68,32 +70,28 @@ avg_time.append(4.9)
 
 df = pd.DataFrame(
     {
-        'test_names': test_names,
-        'in_scene_good_verify': in_scene_good_verify,
-        'exact_good_verify': exact_good_verify,
-        'percent_identified_stars': percent_identified_stars,
-        'avg_time': avg_time,
-    }
+        'Scenario': test_names,
+        '% scenes: star in scene': in_scene_good_verify,
+        '% scenes: star to star': exact_good_verify,
+        '% overall identified stars': percent_identified_stars,
+        'Average time (right)': avg_time,
+    },
 )
-df.set_index("test_names", drop=True, inplace=True)
+df.set_index("Scenario", drop=True, inplace=True)
 
 fig = plt.figure()  # Create matplotlib figure
 
 ax = fig.add_subplot(111)  # Create matplotlib axes
-ax2 = ax.twinx()  # Create another axes that shares the same x-axis as ax.
 
 width = 0.1
 
-df.in_scene_good_verify.plot(
-    kind='bar', color='red', ax=ax, width=width, position=2)
-df.exact_good_verify.plot(
-    kind='bar', color='green', ax=ax, width=width, position=1)
-df.percent_identified_stars.plot(
-    kind='bar', color='blue', ax=ax, width=width, position=0)
-df.avg_time.plot(
-    kind='bar', color='black', ax=ax2, width=width, position=-1)
-
 ax.set_ylabel('Percent [%]')
-ax2.set_ylabel('Time [s]')
+
+ax = df.plot(kind='bar', secondary_y=['Average time (right)'])
+h1, l1 = ax.get_legend_handles_labels()
+h2, l2 = ax.right_ax.get_legend_handles_labels()
+ax.legend(h1+h2, l1+l2, loc='center left')
+plt.title('Scenes time computation and input stars number')
+ax.right_ax.set_ylabel('Time [s]')
 
 plt.show()
--- jetson/jetson_graph.py
+++ jetson/jetson_graph.py
@@ -5,172 +5,110 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from program.const import MAIN_PATH
+from tests.cuda.expected_results_full_startracker import expected_full
 
 
-# TODO clean this mess
+def method_name(break_after, data, power, start_after):
+    names = []
+    for row in data:
+        names.append(row[0].split(' ')[1])
+    names = list(set(names))
+    res = {}
+    for name in names:
+        res[name] = {
+            't': [],
+            'v': []
+        }
+    for row in data:
+        r = row[0].split(' ')
+        if float(r[0]) > break_after:
+            break
+        if float(r[0]) < start_after:
+            continue
+        res[r[1]]['t'].append(float(r[0]) - start_after)
+        res[r[1]]['v'].append(float(r[2]) * power)
+    # names.remove('VDD_SYS_SOC')
+    labels = {
+        'VDD_IN': {
+            'name': 'Total',
+            'color': 'k-'
+        },
+        'VDD_SYS_SOC': {
+            'name': 'SOC',
+            'color': 'g-'
+        },
+        'VDD_SYS_GPU': {
+            'name': 'GPU',
+            'color': 'r-'
+        },
+        'VDD_SYS_CPU': {
+            'name': 'CPU',
+            'color': 'b-'
+        },
+    }
+    return labels, names, res
 
 
 def make_measurement_graph(
-        filename, ylabel, power=1, start_after=3, break_after=33):
-    with open(
-            os.path.join(MAIN_PATH, filename), 'r') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        data = list(csv_reader)
-        names = []
-        for row in data:
-            names.append(row[0].split(' ')[1])
-        names = list(set(names))
-        res = {}
-        for name in names:
-            res[name] = {'t': [], 'v': []}
-        for row in data:
-            r = row[0].split(' ')
-            if float(r[0]) > break_after:
-                break
-            if float(r[0]) < start_after:
-                continue
-            res[r[1]]['t'].append(float(r[0])-start_after)
-            res[r[1]]['v'].append(float(r[2])*power)
-        names.remove('VDD_SYS_SOC')
-        labels = {
-            'VDD_IN': {'name': 'Total', 'color': 'k-'},
-            'VDD_SYS_SOC': {'name': 'SOC', 'color': 'g-'},
-            'VDD_SYS_GPU': {'name': 'GPU', 'color': 'r-'},
-            'VDD_SYS_CPU': {'name': 'CPU', 'color': 'b-'},
-        }
-        for name in names:
-            plt.plot(
-                res[name]['t'], res[name]['v'],
-                labels[name]['color'], label=labels[name]['name'])
+        data, ylabel, power=1, start_after=3, break_after=33):
+    labels, names, res = method_name(break_after, data, power, start_after)
+    for name in names:
+        plt.plot(
+            res[name]['t'], res[name]['v'],
+            labels[name]['color'], label=labels[name]['name'])
 
-        plt.xlabel('Time [s]')
-        plt.ylabel(ylabel)
-        plt.title(filename.split('_')[1].split('.')[0].capitalize())
-        plt.legend(loc=4, ncol=1, borderaxespad=0.)
-        plt.show()
-
-
-def make_time_graph():
-    d = {
-        'centroid': [],
-        'ids': [],
-        'attitude': [],
-        'start_time': [],
-        'end_time': [],
-    }
-    with open(
-            os.path.join(MAIN_PATH, 'times'), 'r') as times_file:
-        lines = times_file.readlines()
-        for l in lines:
-            a = l.replace("\n", "")
-            a = a.replace(" ", "")
-            a = a.split(':')
-            a[1] = float(a[1])
-
-            d[a[0]].append(a[1])
-
-    N = len(d['ids'])
-    ind = np.arange(N)  # the x locations for the groups
-    width = 0.35  # the width of the bars: can also be len(x) sequence
-
-    p1 = plt.bar(ind, d['centroid'], width)
-    p2 = plt.bar(ind, d['ids'], width, bottom=d['centroid'])
-    p3 = plt.bar(ind, d['attitude'], width, bottom=np.array(d['ids'])+np.array(d['centroid']))
-
-    plt.ylabel('Time [s]')
-    # plt.xticks(ind, ('G1', 'G2', 'G3', 'G4', 'G5'))
-    plt.title('Scores by group and gender')
-    plt.xticks(ind, np.arange(1, N+1))
-
-    plt.legend((p1[0], p2[0], p3[0]), ('Centroid', 'IDs', 'Attitude'))
-
+    plt.xlabel('Time [s]')
+    plt.ylabel(ylabel)
+    plt.title(ylabel[:-3])
+    plt.legend(loc=4, ncol=1, borderaxespad=0.)
     plt.show()
 
 
-# make_measurement_graph(
-#     'measurements_current.csv', 'Current [A]', 10 ** -3)  # milliamperes
-# make_measurement_graph(
-#     'measurements_voltage.csv', 'Voltage [V]', 10 ** -3)  # millivolts
-# make_measurement_graph(
-#     'measurements_power.csv', 'Power [W]', 10 ** -3)  # milliwatts
-# make_time_graph()
+def make_time_graph():
+    star_count = [len(s) for s in expected_full]
+    N = len(time_data['ids'])
+    ind = np.arange(N)  # the x locations for the groups
+    width = 0.35  # the width of the bars: can also be len(x) sequence
 
-def find_contiguous_colors(colors):
-    # finds the continuous segments of colors and returns those segments
-    segs = []
-    curr_seg = []
-    prev_color = ''
-    for c in colors:
-        if c == prev_color or prev_color == '':
-            curr_seg.append(c)
-        else:
-            segs.append(curr_seg)
-            curr_seg = []
-            curr_seg.append(c)
-        prev_color = c
-    segs.append(curr_seg)  # the final one
-    return segs
+    fig = plt.figure()  # Create matplotlib figure
 
+    ax = fig.add_subplot(111)  # Create matplotlib axes
+    ax2 = ax.twinx()  # Create another axes that shares the same x-axis as ax.
 
-def plot_multicolored_lines(x, y, colors):
-    segments = find_contiguous_colors(colors)
-    plt.figure()
-    start = 0
-    for seg in segments:
-        end = start + len(seg)
-        l, = plt.gca().plot(
-            x[start:end], y[start:end], lw=2, c=seg[0])
-        start = end
+    p1 = ax.bar(
+        ind, time_data['centroid'], width)
+    p2 = ax.bar(
+        ind, time_data['ids'], width,
+        bottom=time_data['centroid'])
+    p3 = ax.bar(
+        ind, time_data['attitude'], width,
+        bottom=np.array(time_data['ids']) + np.array(time_data['centroid']))
+    p4 = ax2.bar(
+        ind, star_count,
+        color='0.75', width=0.2, align='edge')
+
+    plt.ylabel('Time [s]')
+    plt.title('Scenes time computation and input stars number')
+    plt.xticks(ind, np.arange(1, N + 1))
+
+    plt.legend((p1[0], p2[0], p3[0], p4[0]), (
+        'Star Recognition', 'Star Identification', 'Attitude Estimation',
+        'Number of input stars (right)'
+    ))
+
+    ax.set_ylabel('Time [s]')
+    ax2.set_ylabel('Number of input stars')
+
+    plt.show()
 
 
 def make_power_sectioned_graph(start_time=3):
     start_after = 3
     break_after = 33
     power = 10 ** -3
-    with open(
-            os.path.join(MAIN_PATH, 'measurements_power.csv'), 'r'
-    ) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        data = list(csv_reader)
-        names = []
-        for row in data:
-            names.append(row[0].split(' ')[1])
-        names = list(set(names))
-        res = {}
-        for name in names:
-            res[name] = {'t': [], 'v': []}
-        for row in data:
-            r = row[0].split(' ')
-            if float(r[0]) > break_after:
-                break
-            if float(r[0]) < start_after:
-                continue
-            res[r[1]]['t'].append(float(r[0]) - start_after)
-            res[r[1]]['v'].append(float(r[2]) * power)
-        names.remove('VDD_SYS_SOC')
-        labels = {
-            'VDD_IN': {'name': 'Total', 'color': 'k-'},
-            'VDD_SYS_SOC': {'name': 'SOC', 'color': 'g-'},
-            'VDD_SYS_GPU': {'name': 'GPU', 'color': 'r-'},
-            'VDD_SYS_CPU': {'name': 'CPU', 'color': 'b-'},
-        }
 
-    d = {
-        'centroid': [],
-        'ids': [],
-        'attitude': [],
-        'start_time': [],
-    }
-    with open(
-            os.path.join(MAIN_PATH, 'times'), 'r') as times_file:
-        lines = times_file.readlines()
-        for l in lines:
-            a = l.replace("\n", "")
-            a = a.replace(" ", "")
-            a = a.split(':')
-            a[1] = float(a[1])
-
-            d[a[0]].append(a[1])
+    labels, names, res = method_name(
+        break_after, power_data, power, start_after)
 
     for name in names:
         plt.plot(
@@ -179,16 +117,17 @@ def make_power_sectioned_graph(start_time=3):
 
     end_time = 0
     for i in range(2):
-        for j in range(len(d['start_time'])):
-            if j % len(d['start_time']) == 0:
+        for j in range(len(time_data['start_time'])):
+            if j % len(time_data['start_time']) == 0:
                 color = 'red'
             else:
                 color = 'black'
-            tt = d['start_time'][j]
+            tt = time_data['start_time'][j]
             if tt + start_time + end_time > 30:
                 break
             plt.axvline(tt + start_time + end_time, color=color)
-            if j % len(d['start_time']) == len(d['start_time'])-1:
+            if j % len(time_data['start_time']) == \
+                    len(time_data['start_time']) - 1:
                 end_time = tt + end_time
 
     plt.xlabel('Time [s]')
@@ -199,4 +138,40 @@ def make_power_sectioned_graph(start_time=3):
     plt.show()
 
 
-make_power_sectioned_graph()
+p = os.path.join(MAIN_PATH, 'tests/measurements')
+with open(os.path.join(p, 'measurements_current.csv'), 'r') as current_file, \
+     open(os.path.join(p, 'measurements_voltage.csv'), 'r') as voltage_file, \
+     open(os.path.join(p, 'measurements_power.csv'), 'r') as power_file, \
+     open(os.path.join(p, 'times'), 'r') as times_file:
+    current_reader = csv.reader(current_file, delimiter=',')
+    current_data = list(current_reader)
+    voltage_reader = csv.reader(voltage_file, delimiter=',')
+    voltage_data = list(voltage_reader)
+    power_reader = csv.reader(power_file, delimiter=',')
+    power_data = list(power_reader)
+    times = times_file.readlines()
+
+    time_data = {
+        'centroid': [],
+        'ids': [],
+        'attitude': [],
+        'start_time': [],
+        'end_time': [],
+    }
+
+    for t in times:
+        a = t.replace("\n", "")
+        a = a.replace(" ", "")
+        a = a.split(':')
+        a[1] = float(a[1])
+
+        time_data[a[0]].append(a[1])
+
+    make_measurement_graph(
+        current_data, 'Current [A]', 10 ** -3)  # milliamperes
+    make_measurement_graph(
+        voltage_data, 'Voltage [V]', 10 ** -3)  # millivolts
+    make_measurement_graph(
+        power_data, 'Power [W]', 10 ** -3)  # milliwatts
+    make_time_graph()
+    make_power_sectioned_graph()
